d50378498b36f174cd472ba199217a4fd1653d9b
Antoine Pirlot <antoine.pirlot@student.vinci.be>
2022-03-01 13:10:44 +0100

    Feat: show username on the left navbar while connected

--- webapp/src/Components/Navbar/Navbar.js
+++ webapp/src/Components/Navbar/Navbar.js
@@ -12,48 +12,57 @@
 
 import {getObject} from "../../utils/session";
 
+const navBarHtml = `
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Donnamis</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul id="navbarLinks" class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#" data-uri="/">Home</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div id="usernameNavbar">
+    </div>
+  </nav>
+`;
+
+const loginLinkHtml = `
+          <li class="nav-item">
+            <a class="nav-link" href="#" data-uri="/login">Login</a>
+          </li>
+`;
+
+const logoutLinkHtml = `
+          <li class="nav-item">
+            <a class="nav-link" href="#" data-uri="/logout">Logout</a>
+          </li>
+`;
+
 const Navbar = () => {
   const navbarWrapper = document.querySelector("#navbarWrapper");
+  navbarWrapper.innerHTML = navBarHtml;
   let member = getObject("member");
-  let navbar = `
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Donnamis</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#" data-uri="/">Home</a>
-              </li>`;
-  if (!member) {
-    navbar +=
-        `<li class="nav-item">
-                      <a class="nav-link" href="#" data-uri="/login">Login</a>
-                    </li>`;
+  const links = document.querySelector("#navbarLinks");
+  if (member) {
+    links.innerHTML += logoutLinkHtml;
+    document.querySelector("#usernameNavbar").innerHTML = member.username
   } else {
-    navbar +=
-        `<li class="nav-item">
-                      <a class="nav-link" href="#" data-uri="/logout">Logout</a>
-                    </li>`;
+    links.innerHTML += loginLinkHtml;
   }
-  navbar +=
-      `
-            </ul>
-          </div>
-        </div>
-      </nav>
-  `;
-  navbarWrapper.innerHTML = navbar;
 };
 
 export default Navbar;
