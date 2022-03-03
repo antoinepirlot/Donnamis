fb0380bfc540610d5b4291f14896bc5285e4c7f7
Antoine Pirlot <antoine.pirlot@student.vinci.be>
2022-03-03 12:32:31 +0100

    Refactor: comment GET import

--- src/main/java/be/vinci/pae/ihm/MemberResource.java
+++ src/main/java/be/vinci/pae/ihm/MemberResource.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
+//import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
