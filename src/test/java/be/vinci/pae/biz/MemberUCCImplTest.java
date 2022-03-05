7a22a647bc0ea032803a857bd0cc4b955a20c0e2
Antoine Pirlot <antoine.pirlot@student.vinci.be>
2022-03-05 11:37:32 +0100

    Add: demo test and login test

---
+++ src/test/java/be/vinci/pae/biz/MemberUCCImplTest.java
@@ -0,0 +1,46 @@
+package be.vinci.pae.biz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import be.vinci.pae.dal.MemberDAO;
+import be.vinci.pae.utils.ApplicationBinder;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class MemberUCCImplTest {
+  private MemberDAO memberDAO;
+  private MemberUCC memberUCC;
+  private final ServiceLocator locator = ServiceLocatorUtilities.bind(new ApplicationBinder());
+
+  @BeforeEach
+  void setUp() {
+    memberUCC = new MemberUCCImpl();
+    memberDAO = Mockito.mock(MemberDAO.class);
+  }
+
+  private void configureMemberDTO(String actualState) {
+    MemberDTO memberDTO = new MemberImpl();
+    memberDTO.setActualState(actualState);
+    Mockito.when(memberDAO.getOne("nico", "password"))
+        .thenReturn(memberDTO);
+  }
+
+  @DisplayName("Test getAll")
+  @Test
+  void getAll() {
+    //assertEquals();
+  }
+
+  @Test
+  void loginConfirmedMember() {
+    configureMemberDTO("confirmed");
+    assertDoesNotThrow(
+        () -> memberUCC.login("nico", "password"),
+        "hje"
+    );
+  }
+}
---
+++ src/test/java/be/vinci/pae/utils/ApplicationBinder.java
@@ -0,0 +1,21 @@
+package be.vinci.pae.utils;
+
+import be.vinci.pae.biz.Factory;
+import be.vinci.pae.biz.FactoryImpl;
+import be.vinci.pae.biz.MemberUCC;
+import be.vinci.pae.biz.MemberUCCImpl;
+import be.vinci.pae.dal.MemberDAO;
+import be.vinci.pae.dal.MemberDAOImpl;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.ext.Provider;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.mockito.Mockito;
+
+@Provider
+public class ApplicationBinder extends AbstractBinder {
+
+  @Override
+  protected void configure() {
+    bind(MemberDAOImpl.class).to(Mockito.class).in(Singleton.class);
+  }
+}
