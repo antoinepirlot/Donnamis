e7a58b0044e97b5955981f0e5a201b42e26eaa3b
Antoine Pirlot <antoine.pirlot@student.vinci.be>
2022-03-06 13:41:52 +0100

    Add: tests for refresh token

--- src/test/java/be/vinci/pae/biz/MemberUCCImplTest.java
+++ src/test/java/be/vinci/pae/biz/MemberUCCImplTest.java
@@ -37,6 +37,16 @@ class MemberUCCImplTest {
         .thenReturn(memberDTO);
   }
 
+  private void configureMemberDTO(String actualState) {
+    memberDTO = new MemberImpl();
+    memberDTO.setId(1);
+    memberDTO.setUsername("nico");
+    memberDTO.setActualState(actualState);
+    Mockito.when(memberDAO.getOne("nico"))
+        .thenReturn(memberDTO);
+  }
+
+  @DisplayName("Test login with confirmed member and good password")
   @Test
   void testLoginConfirmedMemberWithGoodPassword() {
     configureMemberDTO("confirmed", password);
@@ -92,6 +102,125 @@ class MemberUCCImplTest {
     assertThrows(
         WebApplicationException.class,
         () -> memberUCC.login("nico", wrongPassword)
+    );
+  }
+
+  @DisplayName("Test refresh token with confirmed member. Request with good username and id")
+  @Test
+  void testRefreshTokenConfirmedMemberWithGoodUsernameAndId() {
+    configureMemberDTO("confirmed");
+    assertDoesNotThrow(
+        () -> memberUCC.getMember("nico", 1)
+    );
+  }
+
+  @DisplayName("Test refresh token with registered member. Request with good username and id")
+  @Test
+  void testRefreshTokenRegisteredMemberWithGoodUsernameAndId() {
+    configureMemberDTO("registered");
+    assertThrows(
+        WebApplicationException.class,
+        () -> memberUCC.getMember("nico", 1)
+    );
+  }
+
+  @DisplayName("Test refresh token with denied member. Request with good username and id")
+  @Test
+  void testRefreshTokenDeniedMemberWithGoodUsernameAndId() {
+    configureMemberDTO("denied");
+    assertThrows(
+        WebApplicationException.class,
+        () -> memberUCC.getMember("nico", 1)
+    );
+  }
+
+  @DisplayName("Test refresh token with confirmed member. Request with good username and wrong id")
+  @Test
+  void testRefreshTokenConfirmedMemberWithGoodUsernameAndWrongId() {
+    configureMemberDTO("confirmed");
+    assertThrows(
+        WebApplicationException.class,
+        () -> memberUCC.getMember("nico", 5)
+    );
+  }
+
+  @DisplayName("Test refresh token with registered member. Request with good username and wrong id")
+  @Test
+  void testRefreshTokenRegisteredMemberWithGoodUsernameAndWrongId() {
+    configureMemberDTO("registered");
+    assertThrows(
+        WebApplicationException.class,
+        () -> memberUCC.getMember("nico", 5)
+    );
+  }
+
+  @DisplayName("Test refresh token with denied member. Request with good username and wrong id")
+  @Test
+  void testRefreshTokenDeniedMemberWithGoodUsernameAndWrongId() {
+    configureMemberDTO("denied");
+    assertThrows(
+        WebApplicationException.class,
+        () -> memberUCC.getMember("nico", 5)
+    );
+  }
+
+  @DisplayName("Test refresh token with confirmed member. Request with wrong username and good id")
+  @Test
+  void testRefreshTokenConfirmedMemberWithWrongUsernameAndGoodId() {
+    configureMemberDTO("confirmed");
+    assertThrows(
+        WebApplicationException.class,
+        () -> memberUCC.getMember("loic", 1)
+    );
+  }
+
+  @DisplayName("Test refresh token with registered member. Request with wrong username and good id")
+  @Test
+  void testRefreshTokenRegisteredMemberWithWrongUsernameAndGoodId() {
+    configureMemberDTO("registered");
+    assertThrows(
+        WebApplicationException.class,
+        () -> memberUCC.getMember("victor", 1)
+    );
+  }
+
+  @DisplayName("Test refresh token with denied member. Request with wrong username and good id")
+  @Test
+  void testRefreshTokenDeniedMemberWithWrongUsernameAndGoodId() {
+    configureMemberDTO("denied");
+    assertThrows(
+        WebApplicationException.class,
+        () -> memberUCC.getMember("antoine", 1)
+    );
+  }
+
+  @DisplayName("Test refresh token with confirmed member. Request with wrong username and id")
+  @Test
+  void testRefreshTokenConfirmedMemberWithWrongUsernameAndId() {
+    configureMemberDTO("confirmed");
+    assertThrows(
+        WebApplicationException.class,
+        () -> memberUCC.getMember("loic", 5)
+    );
+  }
+
+  @DisplayName("Test refresh token with registered member. Request with wrong username and id")
+  @Test
+  void testRefreshTokenRegisteredMemberWithWrongUsernameAndId() {
+    configureMemberDTO("registered");
+    assertThrows(
+        WebApplicationException.class,
+        () -> memberUCC.getMember("victor", 5)
+    );
+  }
+
+  @DisplayName("Test refresh token with denied member. Request with wrong username and id")
+  @Test
+  void testRefreshTokenDeniedMemberWithWrongUsernameAndId() {
+    configureMemberDTO("denied");
+    assertThrows(
+        WebApplicationException.class,
+        () -> memberUCC.getMember("antoine", 5)
     );
   }
 }
