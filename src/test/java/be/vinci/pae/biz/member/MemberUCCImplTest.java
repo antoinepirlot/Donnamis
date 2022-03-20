6aa8738f862213f766b98d33beb6904ea3c9a925
Antoine Pirlot <antone.pirlot@student.vinci.be>
2022-03-20 16:17:42 +0100

    Refactor: add member package in tests

--- src/test/java/be/vinci/pae/biz/member/MemberUCCImplTest.java
+++ src/test/java/be/vinci/pae/biz/member/MemberUCCImplTest.java
@@ -1,4 +1,4 @@
-package be.vinci.pae.biz;
+package be.vinci.pae.biz.member;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
