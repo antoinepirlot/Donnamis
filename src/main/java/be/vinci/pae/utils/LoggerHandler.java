ffbe20a938fbaaa8a4d394ba3af9465235732d2f
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-03-21 20:09:07 +0100

    Refactor: change methods signature

--- src/main/java/be/vinci/pae/utils/LoggerHandler.java
+++ src/main/java/be/vinci/pae/utils/LoggerHandler.java
@@ -1,9 +1,7 @@
 package be.vinci.pae.utils;
 
-import be.vinci.pae.ihm.member.MemberResource;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystemException;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,32 +11,32 @@ public class LoggerHandler {
   private static final String LOG_FILE_PATH = "src/main/java/be/vinci/pae/data/";
   private static final Logger logger = Logger.getGlobal();
 
-  public static void infoLog(String method, Class<? extends MemberResource> className) {
+  public static void infoLog(String method, Class<?> className) {
     infoLog(method, className, "");
   }
 
-  public static void infoLog(String method, Class<? extends MemberResource> className, String message) {
+  public static void infoLog(String method, Class<?> className, String message) {
     logger.log(Level.INFO, getLog(method, className, message));
   }
 
-  public static void severeLog(String method, Class<? extends MemberResource> className) {
+  public static void severeLog(String method, Class<?> className) {
     severeLog(method, className, "");
   }
 
-  public static void severeLog(String method, Class<? extends MemberResource> className, String message) {
+  public static void severeLog(String method, Class<?> className, String message) {
     logger.log(Level.SEVERE, getLog(method, className, message));
   }
 
-  private static String getLog(String method, Class<? extends MemberResource> className, String message) {
+  private static String getLog(String method, Class<?> className, String message) {
     String log = "Method: " + method + " Class: " + className.toString();
     if (!message.equals("")) {
       log += " Message: " + message;
     }
+    File file = new File(LOG_FILE_PATH);
+    if (!file.exists()) {
+      file.mkdir();
+    }
     try {
-      File file = new File(LOG_FILE_PATH);
-      if (!file.exists()) {
-        file.mkdir();
-      }
       FileHandler fileHandler = new FileHandler(LOG_FILE_PATH + className + ".xml");
       logger.addHandler(fileHandler);
     } catch (IOException e) {
