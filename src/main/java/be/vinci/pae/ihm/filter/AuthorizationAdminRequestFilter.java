89b22615338b02a55ceecb4ee2d7ae15071f998b
Antoine Pirlot <antoine.pirlot@student.vinci.be>
2022-03-25 12:34:05 +0100

    Add: check if the member is admin

--- src/main/java/be/vinci/pae/ihm/filter/AuthorizationAdminRequestFilter.java
+++ src/main/java/be/vinci/pae/ihm/filter/AuthorizationAdminRequestFilter.java
@@ -1,14 +1,20 @@
 package be.vinci.pae.ihm.filter;
 
+import be.vinci.pae.biz.member.interfaces.MemberDTO;
 import be.vinci.pae.biz.member.interfaces.MemberUCC;
 import be.vinci.pae.ihm.filter.utils.TokenDecoder;
 import be.vinci.pae.ihm.logs.LoggerHandler;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Singleton
@@ -28,6 +34,17 @@ public class AuthorizationAdminRequestFilter implements ContainerRequestFilter {
 
   @Override
   public void filter(ContainerRequestContext containerRequestContext) throws IOException {
-    TokenDecoder.decodeToken(containerRequestContext);
+    DecodedJWT decodedJWT = TokenDecoder.decodeToken(containerRequestContext);
+    int id = decodedJWT.getClaim("id").asInt();
+    MemberDTO authenticatedMember = memberUCC.getOneMember(id);
+    if (!authenticatedMember.isAdmin()) {
+      String message = "The member with id: " + id + " can't access a admin method";
+      this.logger.log(Level.INFO, message);
+      containerRequestContext.abortWith(Response.status(Status.FORBIDDEN)
+          .entity(message)
+          .type(MediaType.TEXT_PLAIN)
+          .build());
+    }
+    containerRequestContext.setProperty("authorizedMember", authenticatedMember);
   }
 }
