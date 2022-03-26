c4e637dc08749cfbb300c8d6f6ab7caae96f36ad
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-03-26 12:45:40 +0100

    Refactor: move fetch request to a util file

--- webapp/src/Components/Pages/ListMemberPage.js
+++ webapp/src/Components/Pages/ListMemberPage.js
@@ -1,7 +1,11 @@
 /**
  * Render the ListMemberPage
  */
-import {getObject} from "../../utils/session";
+import {
+  confirmAdmin, confirmMember, denyMember,
+  getDeniedMembers,
+  getRegisteredMembers
+} from "../../utils/BackEndRequests";
 
 const ListMemberPage = async () => {
   const pageDiv = document.querySelector("#page");
@@ -12,254 +16,164 @@ const ListMemberPage = async () => {
 
 async function viewRegisteredMembers() {
   const pageDiv = document.querySelector("#page");
-  const options = {
-    method: "GET",
-    headers: {
-      "Authorization": getObject("token")
-    }
-  };
-  try {
-    //Membres en attente d'acceptation
-    const response = await fetch("/api/members/list_registered", options);
-    if (!response.ok) {
-      // status code was not 200, error status code
-      throw new Error(
-          "fetch error : " + response.status + " : " + response.statusText
-      );
-    }
-    const members = await response.json();
 
-    //Create Div and Table
-    const tableWrapperRegistered = document.createElement("div");
-    tableWrapperRegistered.className = "table-responsive pt-5";
-    tableWrapperRegistered.innerHTML = "Membres en attente d'acceptation";
-    const tableRegistered = document.createElement("table");
-    tableRegistered.className = "table table";
-    tableWrapperRegistered.appendChild(tableRegistered);
+  //Membres en attente d'acceptation
+  const members = await getRegisteredMembers();
 
-    //Create the header of the table
-    const thead = document.createElement("thead");
-    const header = document.createElement("tr");
-    thead.appendChild(header);
-    const header1 = document.createElement("th");
-    header1.innerText = "Nom";
-    const header2 = document.createElement("th");
-    header2.innerText = "Prénom";
-    const header3 = document.createElement("th");
-    header3.innerText = "Administrateur";
-    header.appendChild(header1);
-    header.appendChild(header2);
-    header.appendChild(header3);
-    tableRegistered.appendChild(thead);
+  //Create Div and Table
+  const tableWrapperRegistered = document.createElement("div");
+  tableWrapperRegistered.className = "table-responsive pt-5";
+  tableWrapperRegistered.innerHTML = "Membres en attente d'acceptation";
+  const tableRegistered = document.createElement("table");
+  tableRegistered.className = "table table";
+  tableWrapperRegistered.appendChild(tableRegistered);
 
-    // Create the body of the table
-    const tbody = document.createElement("tbody");
+  //Create the header of the table
+  const thead = document.createElement("thead");
+  const header = document.createElement("tr");
+  thead.appendChild(header);
+  const header1 = document.createElement("th");
+  header1.innerText = "Nom";
+  const header2 = document.createElement("th");
+  header2.innerText = "Prénom";
+  const header3 = document.createElement("th");
+  header3.innerText = "Administrateur";
+  header.appendChild(header1);
+  header.appendChild(header2);
+  header.appendChild(header3);
+  tableRegistered.appendChild(thead);
 
-    //For Each Member
-    members.forEach((member) => {
+  // Create the body of the table
+  const tbody = document.createElement("tbody");
 
-      const line = document.createElement("tr");
-      const NameCell = document.createElement("td");
-      NameCell.innerText = member.lastName;
-      line.appendChild(NameCell);
-      const firstNameCell = document.createElement("td");
-      firstNameCell.innerText = member.firstName;
-      line.appendChild(firstNameCell);
+  //For Each Member
+  members.forEach((member) => {
 
-      // Is Admin Button
-      const isAdminButtonCell = document.createElement("td");
-      const isAdminButton = document.createElement("input");
-      isAdminButton.type = "checkbox";
-      isAdminButtonCell.appendChild(isAdminButton);
-      line.appendChild(isAdminButtonCell);
+    const line = document.createElement("tr");
+    const NameCell = document.createElement("td");
+    NameCell.innerText = member.lastName;
+    line.appendChild(NameCell);
+    const firstNameCell = document.createElement("td");
+    firstNameCell.innerText = member.firstName;
+    line.appendChild(firstNameCell);
 
-      // Confirm Button
-      const confirmButtonCell = document.createElement("td");
-      const confirmButton = document.createElement("input");
-      confirmButton.type = "submit";
-      confirmButton.value = "Confirmer";
-      confirmButton.addEventListener("click", async function () {
+    // Is Admin Button
+    const isAdminButtonCell = document.createElement("td");
+    const isAdminButton = document.createElement("input");
+    isAdminButton.type = "checkbox";
+    isAdminButtonCell.appendChild(isAdminButton);
+    line.appendChild(isAdminButtonCell);
 
-        let request;
-        if (isAdminButton.checked) {
-          request = "/api/members/confirmAdmin/";
-        } else {
-          request = "/api/members/confirm/";
-        }
+    // Confirm Button
+    const confirmButtonCell = document.createElement("td");
+    const confirmButton = document.createElement("input");
+    confirmButton.type = "submit";
+    confirmButton.value = "Confirmer";
+    confirmButton.addEventListener("click", async function () {
 
-        //Confirm the registration (Click on the button)
-        try {
-          const options = {
-            method: "PUT",
-            headers: {
-              "Authorization": getObject("token")
-            }
-          };
-          console.log(request);
-          const reponse = await fetch(
-              request + member.id, options);
-          if (!reponse.ok) {
-            throw new Error(
-                "fetch error : " + reponse.status + " : " + reponse.statusText
-            );
-          }
-          location.reload();
-        } catch (error) {
-          console.error("ListMemberPage::error::confirm registration:", error);
-        }
-      });
-      confirmButtonCell.appendChild(confirmButton);
-      line.appendChild(confirmButtonCell);
-
-      //Deny Button
-      const denyButtonCell = document.createElement("td");
-      const denyButton = document.createElement("button");
-      denyButton.innerHTML = "Refuser";
-      denyButton.addEventListener("click", async function () {
-
-        //Confirm the registration (Click on the button)
-        try {
-          const options = {
-            method: "PUT",
-            headers: {
-              "Authorization": getObject("token")
-            }
-          };
-          const reponse = await fetch(
-              "/api/members/denies/" + member.id, options);
-          if (!reponse.ok) {
-            throw new Error(
-                "fetch error : " + reponse.status + " : " + reponse.statusText
-            );
-          }
-          location.reload();
-        } catch (error) {
-          console.error("ListMemberPage::error::deny registration:", error);
-        }
-      });
-      denyButtonCell.appendChild(denyButton);
-      line.appendChild(denyButtonCell);
-
-      line.dataset.memberId = member.id;
-
-      tbody.appendChild(line);
+      //Confirm the registration (Click on the button)
+      if (isAdminButton.checked) {
+        await confirmAdmin();
+      } else {
+        await confirmMember();
+      }
+      location.reload();
     });
-    tableRegistered.appendChild(tbody);
-    // add the HTMLTableElement to the main, within the #page div
+    confirmButtonCell.appendChild(confirmButton);
+    line.appendChild(confirmButtonCell);
 
-    pageDiv.appendChild(tableWrapperRegistered);
-  } catch (error) {
-    console.error("ListMemberPage::error: ", error);
-  }
+    //Deny Button
+    const denyButtonCell = document.createElement("td");
+    const denyButton = document.createElement("button");
+    denyButton.innerHTML = "Refuser";
+    denyButton.addEventListener("click", async function () {
+
+      //Confirm the registration (Click on the button)
+      await denyMember();
+      location.reload();
+    });
+    denyButtonCell.appendChild(denyButton);
+    line.appendChild(denyButtonCell);
+
+    line.dataset.memberId = member.id;
+
+    tbody.appendChild(line);
+  });
+  tableRegistered.appendChild(tbody);
+  // add the HTMLTableElement to the main, within the #page div
+
+  pageDiv.appendChild(tableWrapperRegistered);
 }
 
 async function viewDeniedMembers() {
   const pageDiv = document.querySelector("#page");
-  const options = {
-    method: "GET",
-    headers: {
-      "Authorization": getObject("token")
-    }
-  };
-  try {
-    //Membres en attente d'acceptation
-    const response = await fetch("/api/members/list_denied", options);
-    if (!response.ok) {
-      // status code was not 200, error status code
-      throw new Error(
-          "fetch error : " + response.status + " : " + response.statusText
-      );
-    }
-    const members = await response.json();
+  const members = await getDeniedMembers();
 
-    //Create Div and Table
-    const tableWrapper = document.createElement("div");
-    tableWrapper.className = "table-responsive pt-5";
-    tableWrapper.innerHTML = "Membres refusés";
-    const table = document.createElement("table");
-    table.className = "table table";
-    tableWrapper.appendChild(table);
+  //Create Div and Table
+  const tableWrapper = document.createElement("div");
+  tableWrapper.className = "table-responsive pt-5";
+  tableWrapper.innerHTML = "Membres refusés";
+  const table = document.createElement("table");
+  table.className = "table table";
+  tableWrapper.appendChild(table);
 
-    //Create the header of the table
-    const thead = document.createElement("thead");
-    const header = document.createElement("tr");
-    thead.appendChild(header);
-    const header1 = document.createElement("th");
-    header1.innerText = "Nom";
-    const header2 = document.createElement("th");
-    header2.innerText = "Prénom";
-    const header3 = document.createElement("th");
-    header3.innerText = "Administrateur";
-    header.appendChild(header1);
-    header.appendChild(header2);
-    header.appendChild(header3);
-    table.appendChild(thead);
+  //Create the header of the table
+  const thead = document.createElement("thead");
+  const header = document.createElement("tr");
+  thead.appendChild(header);
+  const header1 = document.createElement("th");
+  header1.innerText = "Nom";
+  const header2 = document.createElement("th");
+  header2.innerText = "Prénom";
+  const header3 = document.createElement("th");
+  header3.innerText = "Administrateur";
+  header.appendChild(header1);
+  header.appendChild(header2);
+  header.appendChild(header3);
+  table.appendChild(thead);
 
-    // Create the body of the table
-    const tbody = document.createElement("tbody");
-    members.forEach((member) => {
-      const line = document.createElement("tr");
-      const NameCell = document.createElement("td");
-      NameCell.innerText = member.lastName;
-      line.appendChild(NameCell);
-      const firstNameCell = document.createElement("td");
-      firstNameCell.innerText = member.firstName;
-      line.appendChild(firstNameCell);
+  // Create the body of the table
+  const tbody = document.createElement("tbody");
+  members.forEach((member) => {
+    const line = document.createElement("tr");
+    const NameCell = document.createElement("td");
+    NameCell.innerText = member.lastName;
+    line.appendChild(NameCell);
+    const firstNameCell = document.createElement("td");
+    firstNameCell.innerText = member.firstName;
+    line.appendChild(firstNameCell);
 
-      // Is Admin Button
-      const isAdminButtonCell = document.createElement("td");
-      const isAdminButton = document.createElement("input");
-      isAdminButton.type = "checkbox";
-      isAdminButtonCell.appendChild(isAdminButton);
-      line.appendChild(isAdminButtonCell);
+    // Is Admin Button
+    const isAdminButtonCell = document.createElement("td");
+    const isAdminButton = document.createElement("input");
+    isAdminButton.type = "checkbox";
+    isAdminButtonCell.appendChild(isAdminButton);
+    line.appendChild(isAdminButtonCell);
 
-      // Confirm Button
-      const confirmButtonCell = document.createElement("td");
-      const confirmButton = document.createElement("button");
-      confirmButton.innerHTML = "Confirmer";
-      confirmButton.addEventListener("click", async function () {
+    // Confirm Button
+    const confirmButtonCell = document.createElement("td");
+    const confirmButton = document.createElement("button");
+    confirmButton.innerHTML = "Confirmer";
+    confirmButton.addEventListener("click", async function () {
 
-        let request;
-        if (isAdminButton.checked) {
-          request = "/api/members/confirmAdmin/";
-        } else {
-          request = "/api/members/confirm/";
-        }
-
-        //Confirm the registration (Click on the button)
-        try {
-          const options = {
-            method: "PUT",
-            headers: {
-              "Authorization": getObject("token")
-            }
-          };
-
-
-          const reponse = await fetch(
-              request + member.id, options);
-          if (!reponse.ok) {
-            throw new Error(
-                "fetch error : " + reponse.status + " : " + reponse.statusText
-            );
-          }
-          location.reload();
-        } catch (error) {
-          console.error("ListMemberPage::error::confirm registration:", error);
-        }
-      });
-      confirmButtonCell.appendChild(confirmButton);
-      line.appendChild(confirmButtonCell);
-
-      line.dataset.memberId = member.id;
-      tbody.appendChild(line);
+      //Confirm the registration (Click on the button)
+      if (isAdminButton.checked) {
+        await confirmAdmin();
+      } else {
+        await confirmMember();
+      }
+      location.reload();
     });
-    table.appendChild(tbody);
-    // add the HTMLTableElement to the main, within the #page div
-    pageDiv.appendChild(tableWrapper);
-  } catch (error) {
-    console.error("ListMemberPage::error: ", error);
-  }
+    confirmButtonCell.appendChild(confirmButton);
+    line.appendChild(confirmButtonCell);
+
+    line.dataset.memberId = member.id;
+    tbody.appendChild(line);
+  });
+  table.appendChild(tbody);
+  // add the HTMLTableElement to the main, within the #page div
+  pageDiv.appendChild(tableWrapper);
+
 }
 
 export default ListMemberPage;
---
+++ webapp/src/utils/BackEndRequests.js
@@ -0,0 +1,106 @@
+import {getObject, getPayload} from "./session";
+
+async function getRegisteredMembers() {
+  try {
+    const options = {
+      method: "GET",
+      headers: {
+        "Authorization": getObject("token")
+      }
+    };
+
+    const response = await fetch("/api/members/list_registered", options);
+    if (!response.ok) {
+      // status code was not 200, error status code
+      throw new Error(
+          "fetch error : " + response.status + " : " + response.statusText
+      );
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("ListMemberPage::error: ", error);
+  }
+}
+
+async function getDeniedMembers() {
+  const options = {
+    method: "GET",
+    headers: {
+      "Authorization": getObject("token")
+    }
+  };
+  try {
+    //Membres en attente d'acceptation
+    const response = await fetch("/api/members/list_denied", options);
+    if (!response.ok) {
+      // status code was not 200, error status code
+      throw new Error(
+          "fetch error : " + response.status + " : " + response.statusText
+      );
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("ListMemberPage::error: ", error);
+  }
+}
+
+async function confirmMember() {
+  const request = {
+    method: "PUT",
+    headers: {
+      "Authorization": getObject("token")
+    }
+  };
+  const url = "/api/members/confirm/";
+  const response = await fetch(url + await getPayload().id, request);
+  if (!response.ok) {
+    throw new Error(
+        "fetch error : " + response.status + " : " + response.statusText
+    );
+  }
+}
+
+async function confirmAdmin() {
+  const request = {
+    method: "PUT",
+    headers: {
+      "Authorization": getObject("token")
+    }
+  };
+  const payload = await getPayload();
+  const url = "/api/members/confirmAdmin/" + payload.id;
+  const response = await fetch(url, request);
+  if (!response.ok) {
+    throw new Error(
+        "fetch error : " + response.status + " : " + response.statusText
+    );
+  }
+}
+
+async function denyMember() {
+  try {
+    const request = {
+      method: "PUT",
+      headers: {
+        "Authorization": getObject("token")
+      }
+    };
+    const url = "/api/members/denies/" + await getPayload().id;
+    const reponse = await fetch(url, request);
+    if (!reponse.ok) {
+      throw new Error(
+          "fetch error : " + reponse.status + " : " + reponse.statusText
+      );
+    }
+  } catch (error) {
+    console.error("ListMemberPage::error::deny registration:", error);
+  }
+}
+
+export {
+  getRegisteredMembers,
+  getDeniedMembers,
+  confirmMember,
+  confirmAdmin,
+    denyMember
+};
