1f0f3822e9da85e5662131e8bda40a94667cd03f
Antoine Pirlot <antone.pirlot@student.vinci.be>
2022-03-26 23:41:38 +0100

    Refactor: add missing javadoc

--- src/main/java/be/vinci/pae/biz/offer/interfaces/OfferUCC.java
+++ src/main/java/be/vinci/pae/biz/offer/interfaces/OfferUCC.java
@@ -4,11 +4,33 @@ import java.util.List;
 
 public interface OfferUCC {
 
+  /**
+   * Asks UCC to create an offer.
+   *
+   * @param offerDTO the offer to create
+   * @return true if the offer has been created otherwise false
+   */
   boolean createOffer(OfferDTO offerDTO);
 
+  /**
+   * Asks UCC to get a list of the latest offers.
+   *
+   * @return the list of the latest offers
+   */
   List<OfferDTO> getLatestOffers();
 
+  /**
+   * Asks UCC to get a list of all offers.
+   *
+   * @return the list of all offers
+   */
   List<OfferDTO> getAllOffers();
 
+  /**
+   * Asks UCC to get one offer identified by its id.
+   *
+   * @param id the offer's id
+   * @return the offer if it exists, otherwise null
+   */
   OfferDTO getOneOffer(int id);
 }
