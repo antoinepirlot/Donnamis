2093701468ba4ea9d2ab23e9d4f4dc60d39322cc
Antoine Pirlot <antone.pirlot@student.vinci.be>
2022-03-26 18:32:33 +0100

    Refactor: move the member test to the good package

--- src/test/java/be/vinci/pae/biz/member/objects/MemberUCCImplTest.java
+++ src/test/java/be/vinci/pae/biz/member/objects/MemberUCCImplTest.java
@@ -1,4 +1,4 @@
-package be.vinci.pae.biz.member;
+package be.vinci.pae.biz.member.objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
