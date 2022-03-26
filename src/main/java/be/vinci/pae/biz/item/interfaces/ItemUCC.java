e2a33d14ecad408e037334b342d97c50deb63ba6
Antoine Pirlot <antone.pirlot@student.vinci.be>
2022-03-26 23:33:27 +0100

    Refactor: add missing javadoc

--- src/main/java/be/vinci/pae/biz/item/interfaces/ItemUCC.java
+++ src/main/java/be/vinci/pae/biz/item/interfaces/ItemUCC.java
@@ -4,8 +4,16 @@ import java.util.List;
 
 public interface ItemUCC {
 
+  /**
+   * Ask itemDAO to give a list of the latest items.
+   * @return a list of the latest items
+   */
   List<ItemDTO> getLatestItems();
 
+  /**
+   * Ask itemDAO to give a list of all items.
+   * @return a list of all items
+   */
   List<ItemDTO> getAllItems();
 
   /**
@@ -14,9 +22,21 @@ public interface ItemUCC {
    */
   List<ItemDTO> getAllOfferedItems();
 
+  /**
+   * Ask itemDAO to get one item identified by its id.
+   * @return the item associated by the id
+   */
   ItemDTO getOneItem(int id);
 
+  /**
+   * Ask itemDAO to add the item.
+   * @return true if the adding is a success, otherwise false
+   */
   boolean addItem(ItemDTO itemDTO);
 
+  /**
+   * Ask itemDAO to cancel the item identified by its id.
+   * @return the cancelled item
+   */
   ItemDTO cancelOffer(int id);
 }
