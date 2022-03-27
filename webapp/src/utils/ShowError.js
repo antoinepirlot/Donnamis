c364d625cbcad2a729f0edaab07b12c25a3b0c19
Nicolas Dimitriadis <nicolas.dimitriadis@student.vinci.be>
2022-03-27 12:37:45 +0200

    Refactor : print max one error login message (Bug Fixed)

--- webapp/src/utils/ShowError.js
+++ webapp/src/utils/ShowError.js
@@ -1,6 +1,5 @@
-
-function showError(message, alertType, element){
-  element.innerHTML += `
+function showError(message, alertType, element) {
+  element.innerHTML = `
     <p class="alert-${alertType}">${message}</p>
   `;
 }
