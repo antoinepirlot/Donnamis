30a8af62cdaea8b2d732b79e9e78fbbc42f199bd
Antoine Pirlot <antone.pirlot@student.vinci.be>
2022-03-27 10:50:42 +0200

    Refactor: update init.sql for s8

--- init.sql
+++ init.sql
@@ -25,7 +25,7 @@ CREATE TABLE project_pae.addresses
     id_address      SERIAL PRIMARY KEY,
     street          VARCHAR(150) NOT NULL,
     building_number VARCHAR(20)  NOT NULL,
-    unit_number     VARCHAR(10)  NULL,
+    unit_number     VARCHAR(10) NULL,
     postcode        VARCHAR(10)  NOT NULL,
     commune         VARCHAR(100) NOT NULL,
     id_member       INTEGER      NOT NULL,
@@ -95,60 +95,119 @@ CREATE TABLE project_pae.recipients
 
 );
 -------------------INSERT INTO--------------------------
+-- MEMBERS TABLE
 --Mot de passe : password
 INSERT INTO project_pae.members (username, password, last_name, first_name, is_admin, state, phone)
-VALUES ('nikesakou', '$2a$10$vD5FXSmaNv4DkfpFfKfDsOjaJ192x2RdWyjIWr28lj5r1X9uvB9yC', 'Dimitriadis',
-        'Nicolas', true, 'confirmed', NULL);
-
+VALUES ('caro', '$2a$10$xakKOBvypQxUzlYFc9Nhz.fJ6voiJsnGzKpmelMZnqJWB5GLTibGq', 'Line',
+        'Caroline', false, 'denied', NULL);
 INSERT INTO project_pae.members (username, password, last_name, first_name, is_admin, state, phone)
-VALUES ('lepirelot', '$2a$10$S6YG0YYAwZ.MLLsCOlhUiuy6HYl.U.r3dG60HnopHfY4RIpPK8jrS', 'Pirlot',
-        'Antoine', true, 'registered', NULL);
-
+VALUES ('achil', '$2a$10$HNgw3caDAFIA4vr4U6sW7OOSZORCLLf./yQOnYxYvbWir88ZTDzZ6', 'Ile',
+        'Achille', false, 'registered', NULL);
 INSERT INTO project_pae.members (username, password, last_name, first_name, is_admin, state, phone)
-VALUES ('Navial', '$2a$10$Psvm5269Z5F.eR1LRz92feZ/s1Euxsi15yBkLRFWJkX0PGhidAFP6', 'Denis', 'Victor',
-        false, 'confirmed', '0479489137');
+VALUES ('bazz', '$2a$10$1biHuxC0nR6Vvjyj8NW9XedY3LCCcwbQyAkCUG8IJmfte/OU4Kxfm', 'Ile', 'Basile',
+        false, 'confirmed', NULL);
 INSERT INTO project_pae.members (username, password, last_name, first_name, is_admin, state, phone)
-VALUES ('Swapiz', '$2a$10$YUc0XQ56mZr9p2RQOPko/OuszOaUJaypZ.viL4FSFwG4oeKkoQNZS', 'Hernaut', 'Loic',
-        false, 'confirmed', '0478249544');
+VALUES ('bri', '$2a$10$gafrRJ.P4GA8TkHkcCgPjuB.U5okgAOn3jXeA/HPAe7xqE89PYuhy', 'Lehmann',
+        'Brigitte', true, 'confirmed', NULL);
 
-INSERT INTO project_pae.members (username, password, last_name, first_name, is_admin, state, phone)
-VALUES ('username', '$2a$10$EOyq/phHFeZu4dnCJIpS2e4GBwOrhMOkY3Oz5zIQWqjN23ziS1Wra', 'Lastname',
-        'Firstname', false, 'denied', NULL);
+-- ADDRESS TABLE
+INSERT INTO project_pae.addresses (street, building_number, unit_number, postcode, commune,
+                                   id_member)
+VALUES ('Rue de l’Eglise', '11', 'B1', '4987', 'Stroumont', 1);
+INSERT INTO project_pae.addresses (street, building_number, postcode, commune, id_member)
+VALUES ('Rue de Renkin', '7', '4800', 'Verviers', 2);
+INSERT INTO project_pae.addresses (street, building_number, unit_number, postcode, commune,
+                                   id_member)
+VALUES ('Rue Haute Folie', '6', 'A103', '4800', 'Verviers', 3);
+INSERT INTO project_pae.addresses (street, building_number, postcode, commune, id_member)
+VALUES ('Haut-Vinâve', '13', '4845', 'Jalhay', 4);
 
+-- REFUSALS TABLE
+INSERT INTO project_pae.refusals (text, id_member)
+VALUES ('Il faudra patienter un jour ou deux.', 1);
+
+-- ITEMS TYPE TABLE
 INSERT INTO project_pae.items_types (item_type)
-VALUES ('Décoration');
+VALUES ('Accessoires pour animaux domestiques'); -- 1
+INSERT INTO project_pae.items_types (item_type)
+VALUES ('Accessoire pour voiture'); -- 2
+INSERT INTO project_pae.items_types (item_type)
+VALUES ('Décoration'); -- 3
+INSERT INTO project_pae.items_types (item_type)
+VALUES ('Jouets'); -- 4
+INSERT INTO project_pae.items_types (item_type)
+VALUES ('Literie'); -- 5
+INSERT INTO project_pae.items_types (item_type)
+VALUES ('Matériel de cuisine'); -- 6
+INSERT INTO project_pae.items_types (item_type)
+VALUES ('Matériel de jardinage'); -- 7
+INSERT INTO project_pae.items_types (item_type)
+VALUES ('Meuble'); -- 8
+INSERT INTO project_pae.items_types (item_type)
+VALUES ('Plantes'); -- 9
+INSERT INTO project_pae.items_types (item_type)
+VALUES ('Produits cosmétiques'); -- 10
+INSERT INTO project_pae.items_types (item_type)
+VALUES ('Vélo trotinette'); -- 11
+INSERT INTO project_pae.items_types (item_type)
+VALUES ('Vêtements');
+-- 12
 
+-- ITEMS TABLE
 INSERT INTO project_pae.items (item_description, id_item_type, id_member, photo, title,
                                offer_status)
-VALUES ('Cadre de la mort qui tue', 1, 1, 'photo', 'Cadre', 'donated');
+VALUES ('Décorations de Noël de couleur rouge', 3, 3, 'christmas-1869533_640', 'Titre', 'donated');
+INSERT INTO project_pae.items (item_description, id_item_type, id_member, photo, title,
+                               offer_status)
+VALUES ('Cadre représentant un chien noir sur un fond noir.', 3, 3, 'dog-4118585_640', 'Titre',
+        'donated');
+INSERT INTO project_pae.items (item_description, id_item_type, id_member, photo, title,
+                               offer_status)
+VALUES ('Ancien bureau d’écolier.', 8, 4, 'BureauEcolier-7', 'Titre', 'donated');
 
+-- OFFERS TABLE
+INSERT INTO project_pae.offers ("date", time_slot, id_item)
+VALUES ('21-03-22', 'Mardi de 17h à 22h', 1);
+INSERT INTO project_pae.offers ("date", time_slot, id_item)
+VALUES ('25-03-22', 'Lundi de 18h à 22h', 2);
+INSERT INTO project_pae.offers ("date", time_slot, id_item)
+VALUES ('25-03-22', 'Tous les jours de 15h à 18h', 3);
 
-SELECT offers.id_offer,
-       offers.date,
-       offers.time_slot,
-       items.id_item,
-       items.item_description,
-       items.id_item_type,
-       items.id_member,
-       items.photo,
-       items.title,
-       items.offer_status
-FROM project_pae.offers offers
-         LEFT OUTER JOIN project_pae.items items ON offers.id_item = items.id_item
-ORDER BY offers.date DESC;
+-- SELECT
+SELECT m.id_member,
+       m.username,
+       m.is_admin,
+       m.state,
+       m.phone,
+       r.text
+FROM project_pae.members m,
+     project_pae.refusals r
+WHERE m.id_member = r.id_member
+ORDER BY m.state;
 
-SELECT item.title,
-       item.item_description,
-       item_type.item_type,
-       offer.date,
-       offer.time_slot,
-       member.first_name,
-       member.last_name
-      FROM project_pae.items item, project_pae.items_types item_type, project_pae.offers offer, project_pae.members member
-      WHERE item.id_item_type = item_type.id_type AND
-            item.id_item = offer.id_item AND
-            item.id_member = member.id_member AND
-            offer.id_offer = 1;
+SELECT i.id_item,
+       i.item_description,
+       it.item_type,
+       i.offer_status,
+       min(o.date) AS "Date de la première offre"
+FROM project_pae.items i,
+     project_pae.items_types it,
+     project_pae.offers o
+WHERE i.id_item_type = it.id_type
+  AND i.id_item = o.id_item
+GROUP BY i.id_item,
+         i.item_description,
+         it.item_type,
+         i.offer_status
+ORDER BY 5 DESC;
 
-
+SELECT m.last_name,
+       i.item_description
+FROM project_pae.items i,
+     project_pae.members m
+WHERE i.id_member = m.id_member
+GROUP BY m.last_name,
+         i.item_description
+ORDER BY m.last_name,
+         i.item_description;
 
