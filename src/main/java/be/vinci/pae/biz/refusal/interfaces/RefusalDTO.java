94ee7ba73f7310184c73aff653afcc4fec5f4ba6
Antoine Pirlot <antone.pirlot@student.vinci.be>
2022-04-15 22:06:06 +0200

    Feat: add refusals classes

---
+++ src/main/java/be/vinci/pae/biz/refusal/interfaces/RefusalDTO.java
@@ -0,0 +1,21 @@
+package be.vinci.pae.biz.refusal.interfaces;
+
+import be.vinci.pae.biz.member.interfaces.MemberDTO;
+import be.vinci.pae.biz.refusal.objects.RefusalImpl;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(as = RefusalImpl.class)
+public interface RefusalDTO {
+
+  int getIdRefusal();
+
+  void setIdRefusal(int idRefusal);
+
+  MemberDTO getMember();
+
+  void setMember(MemberDTO member);
+
+  String getText();
+
+  void setText(String text);
+}
