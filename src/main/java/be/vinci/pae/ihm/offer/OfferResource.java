4f1b7ee8b23f90c727e10df41bd7bc166c93fd8b
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-04-16 16:13:51 +0200

    Refactor: replace exceptions with implemented exceptions

--- src/main/java/be/vinci/pae/ihm/offer/OfferResource.java
+++ src/main/java/be/vinci/pae/ihm/offer/OfferResource.java
@@ -3,6 +3,7 @@ package be.vinci.pae.ihm.offer;
 import be.vinci.pae.biz.offer.interfaces.OfferDTO;
 import be.vinci.pae.biz.offer.interfaces.OfferUCC;
 import be.vinci.pae.exceptions.webapplication.ConflictException;
+import be.vinci.pae.exceptions.webapplication.ObjectNotFoundException;
 import be.vinci.pae.exceptions.webapplication.WrongBodyDataException;
 import be.vinci.pae.ihm.filter.AuthorizeMember;
 import be.vinci.pae.ihm.filter.utils.Json;
@@ -13,9 +14,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -68,8 +67,7 @@ public class OfferResource {
   public List<OfferDTO> getLatestOffers() throws SQLException {
     List<OfferDTO> listOfferDTO = offerUCC.getLatestOffers();
     if (listOfferDTO == null) {
-      throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-          .entity("Ressource not found").type("text/plain").build());
+      throw new ObjectNotFoundException("No offers into the database.");
     }
     return this.jsonUtil.filterPublicJsonViewAsList(listOfferDTO);
   }
@@ -88,8 +86,7 @@ public class OfferResource {
   public List<OfferDTO> getAllOffers() throws SQLException {
     List<OfferDTO> listOfferDTO = offerUCC.getAllOffers();
     if (listOfferDTO == null) {
-      throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-          .entity("Ressource not found").type("text/plain").build());
+      throw new ObjectNotFoundException("No offers into the database.");
     }
     return this.jsonUtil.filterPublicJsonViewAsList(listOfferDTO);
   }
