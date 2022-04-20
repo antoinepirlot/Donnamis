a920e640ee7a4dfa9a3361117cc0ed7a09d779cd
victor denis <you@example.com>
2022-04-20 16:18:55 +0200

    feat: getting started with image upload

---
+++ src/main/java/be/vinci/pae/image/ImageRessource.java
@@ -0,0 +1,35 @@
+package be.vinci.pae.image;
+
+import be.vinci.pae.ihm.filter.AuthorizeMember;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+import org.apache.maven.surefire.shared.io.FilenameUtils;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+public class ImageRessource {
+
+  @POST
+  @Path("/upload")
+  @Consumes(MediaType.MULTIPART_FORM_DATA)
+  @AuthorizeMember
+  public Response uploadFile(@FormDataParam("file") InputStream file,
+      @FormDataParam("file") FormDataContentDisposition fileDisposition) throws IOException {
+    //UUID uuid = UUID.randomUUID();
+    String fileName = fileDisposition.getFileName();
+    Files.copy(file, Paths.get(fileName));
+
+    String extension = FilenameUtils.getExtension(fileDisposition.getName());
+    return Response.ok().build();
+  }
+
+
+}
