f8f3097e015f91ec49c0a2cfa0a897e9db95c119
Loïc Hernaut <loic.hernaut@student.vinci.be>
2022-04-20 13:51:54 +0200

    Add : First Version Frontend

--- webapp/src/Components/Pages/ViewItemPage.js
+++ webapp/src/Components/Pages/ViewItemPage.js
@@ -1,10 +1,12 @@
-import {getPayload,} from "../../utils/session";
+import {getObject, getPayload,} from "../../utils/session";
 import {Redirect} from "../Router/Router";
 import {showError} from "../../utils/ShowError";
 import {
   getItem,
+  modifyTheItem,
   postInterest as postInterestBackEnd
 } from "../../utils/BackEndRequests";
+import {openModal} from "../../utils/Modals";
 
 const viewOfferHtml = `
 <div id="offerCard" class="card mb-3">
@@ -19,11 +21,8 @@ const viewOfferHtml = `
         <h5 id="pubDate" class="card-text"></h5>
 
         <form>
-                <div class="form-check form-switch">
-         <input class="form-check-input" type="checkbox" id="callWanted">
-         <label class="form-check-label" for="callWanted">J'accepte d'être appelé</label>
-        </div>
-           <input id="interestButton" type="submit" class="btn btn-primary" value="Je suis interessé(e) !">
+          <div id="call_wanted"></div>
+           <input id="interestButton" type="submit" class="btn btn-primary">
        </form>
 
        <div class="message" id="interestMessage"></div>
@@ -33,6 +32,26 @@ const viewOfferHtml = `
       <img src="" class="card-img" alt="JS">
     </div>
   </div>
+</div>
+<!-- The Modal -->
+<div id="modifyItemModal" class="modal">
+  <div class="modal-content">
+    <span id="modifyItemCloseModal" class="close">&times;</span>
+    <form id="modifyItemForm">
+      <h5>Modifier votre objet</h5><br>
+      <p>Nom de l'objet</p>
+      <input id="titleForm" type="text">
+      <p>Description de l'objet</p>
+      <input id="itemDescriptionForm" type="text">
+      <p>Photo</p>
+      <input id="photoForm" type="file">
+      <p>Type de l'objet</p>
+      <input id="itemTypeFormList" list="itemsTypes"><br>
+      <p></p>
+      <input type="submit" value="Modifier">
+    </form>
+  </div>
+  <div id="errorMessage"></div>
 </div>
 `;
 
@@ -52,11 +71,27 @@ async function ViewItemPage() {
   const page = document.querySelector("#page");
   const idItem = urlParams.get("id");
   page.innerHTML = viewOfferHtml;
-  const button = document.querySelector("#offerCard");
+  const form = document.querySelector("#offerCard");
   //get offer's infos with the id in param
-  await getItemInfo(idItem);
-  //post an interest
-  button.addEventListener("submit", postInterest);
+
+  const item = await getItemInfo(idItem);
+  console.log(item);
+  const modifyMember = getObject("memberDTO");
+  const button = document.querySelector("#interestButton");
+
+  if (item.member.id == modifyMember.id) {
+    button.value = "Modifier";
+    //modify item
+    form.addEventListener("submit", showModifyForm);
+  } else {
+    const call_wanted = document.querySelector("#call_wanted");
+    call_wanted.innerHTML = ` 
+        <input class=\"form-check-input\" type=\"checkbox\" id=\"callWanted\">
+        <label class=\"form-check-label\" for=\"callWanted\">J'accepte d'être appelé</label>`;
+    button.value = "Je suis interessé(e) !";
+    //post an interest
+    form.addEventListener("submit", postInterest);
+  }
 }
 
 async function getItemInfo(idItem) {
@@ -78,13 +113,13 @@ async function getItemInfo(idItem) {
         "#availabilities").innerHTML = `Disponibilités : ${lastOffer.timeSlot}`
     document.querySelector(
         "#pubDate").innerHTML = `Date de publication : ${date}`
+    return item;
   } catch (err) {
     console.error(err);
   }
 }
 
 async function postInterest(e) {
-  console.log("postInterest");
   e.preventDefault();
   const interestMessage = document.querySelector("#interestMessage");
   //const urlParams = new URLSearchParams(queryString);
@@ -106,6 +141,43 @@ async function postInterest(e) {
     console.error(err);
   }
 
+}
+
+async function showModifyForm(e) {
+  e.preventDefault();
+
+  openModal("#modifyItemModal", "#modifyItemCloseModal");
+  const modifyForm = document.querySelector("#modifyItemForm");
+  modifyForm.addEventListener("submit", modifyItem);
+}
+
+async function modifyItem(e) {
+  e.preventDefault();
+
+  const title = document.querySelector("#titleForm");
+  const itemDescription = document.querySelector("#itemDescriptionForm");
+  const photo = document.querySelector("#photoForm");
+  const newItemType = document.querySelector("#itemTypeForm");
+
+  const itemType = {
+    itemType: newItemType
+  }
+
+  const item = {
+    id: 4,
+    itemDescription: itemDescription,
+    itemType: itemType,
+    photo: photo,
+    title: title,
+  }
+
+  try {
+    await modifyTheItem(item);
+    const errorMessage = document.querySelector("#errorMessage");
+    showError("Modification validé", "success", errorMessage);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default ViewItemPage;
--- webapp/src/utils/BackEndRequests.js
+++ webapp/src/utils/BackEndRequests.js
@@ -279,6 +279,25 @@ async function modifyMember(id, member) {
   return response.ok;
 }
 
+async function modifyTheItem(item) {
+  const request = {
+    method: "PUT",
+    headers: {
+      "Authorization": getObject("token"),
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(item)
+  };
+  const response = await fetch("/api/items/modify", request);
+  if (!response.ok) {
+    showError("Impossible de modifier l'objet");
+    throw new Error(
+        "fetch error : " + response.status + " : " + response.statusText
+    );
+  }
+  return response.ok;
+}
+
 async function offerAgain(offer) {
   const request = {
     method: "POST",
@@ -377,4 +396,5 @@ export {
   offerAgain,
   postInterest,
   modifyMember,
+  modifyTheItem,
 };
