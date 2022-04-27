e2a7f7ec0cbccc4ec4bc6acb8a3403a12db24e6a
Victor Denis <victor.denis@student.vinci.com>
2022-04-27 16:38:14 +0200

    Style: Increase the readability of the navbar

--- webapp/src/Components/Navbar/Navbar.js
+++ webapp/src/Components/Navbar/Navbar.js
@@ -58,24 +58,6 @@ const logoutLinkHtml = `
           </li>
 `;
 
-const listMemberLinkHtml = `
-          <li class="nav-item">
-            <a class="nav-link" href="#" data-uri="/list_member">Liste des membres</a>
-          </li>
-`;
-
-const searchMembersLinkHtml = `
-          <li class="nav-item">
-            <a class="nav-link" href="#" data-uri="/search_members">Rechercher des membres</a>
-          </li>
-`;
-
-const allItemsLinkHtml = `
-          <li class="nav-item">
-            <a class="nav-link" href="#" data-uri="/all_items">Tous les objets</a>
-          </li>
-`;
-
 const allOfferedItemsLinkHtml = `
           <li class="nav-item">
             <a class="nav-link" href="#" data-uri="/all_offered_items">Tous les objets offerts</a>
@@ -103,6 +85,29 @@ const myAssignedItemsLinkHtml = `
   </li>
 `
 
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////---ADMIN PAGES---////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+const listMemberLinkHtml = `
+          <li class="nav-item" id="admin-page" >
+            <a  class="nav-link" href="#" data-uri="/list_member">Liste des membres</a>
+          </li>
+`;
+
+const searchMembersLinkHtml = `
+          <li class="nav-item" id="admin-page">
+            <a class="nav-link" href="#" data-uri="/search_members">Rechercher des membres</a>
+          </li>
+`;
+
+const allItemsLinkHtml = `
+          <li class="nav-item" id="admin-page">
+            <a class="nav-link" href="#" data-uri="/all_items">Tous les objets</a>
+          </li>
+`;
+
+
 const Navbar = async () => {
   const navbarWrapper = document.querySelector("#navbarWrapper");
   navbarWrapper.innerHTML = navBarHtml;
@@ -113,17 +118,17 @@ const Navbar = async () => {
     naveBarMemberPlace.innerHTML = profilLinkHtml;
     const memberUsername = document.querySelector("#memberUsername");
     memberUsername.innerHTML += memberDTO.username;
+
+    links.innerHTML += allOfferedItemsLinkHtml;
+    links.innerHTML += offerAnItemLinkHtml;
+    links.innerHTML += myItemsLinkHtml;
+    links.innerHTML += myAssignedItemsLinkHtml
     if (isAdmin()) {
       links.innerHTML += listMemberLinkHtml;
       links.innerHTML += searchMembersLinkHtml;
       links.innerHTML += allItemsLinkHtml;
       memberUsername.innerHTML = memberDTO.username + " (admin)";
     }
-    links.innerHTML += allOfferedItemsLinkHtml;
-    links.innerHTML += offerAnItemLinkHtml;
-    links.innerHTML += myItemsLinkHtml;
-    links.innerHTML += myAssignedItemsLinkHtml
-
     links.innerHTML += logoutLinkHtml;
   } else {
     links.innerHTML += loginLinkHtml;
