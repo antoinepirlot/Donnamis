70de63a9c5471256db769c7c90c14fd5a36ef200
Antoine Pirlot <antoine.pirlot@student.vinci.be>
2022-04-28 19:32:16 +0200

    Refactor: re put modal on home page if not connected

--- webapp/src/Components/Pages/Public/HomePage.js
+++ webapp/src/Components/Pages/Public/HomePage.js
@@ -1,5 +1,5 @@
 import {getAllItemsByOfferStatus} from "../../../utils/BackEndRequests";
-import {getShowItemsHtml} from "../../../utils/HtmlCode";
+import {checkIfMemberLoggedIn, getShowItemsHtml} from "../../../utils/HtmlCode";
 import {getPayload} from "../../../utils/session";
 
 const tableHtml = `
@@ -11,6 +11,16 @@ const tableHtml = `
      <div id="searchBarHomePage">
      </div> 
     <div class="row" id="all_offered_items">
+    </div>
+  </div>
+  
+  <!-- The Modal ask if the member want to log in or register -->
+  <div id="homePageModal" class="modal">
+    <div class="modal-content">
+      <span id="homePageModalCloseButton" class="close">&times;</span>
+      <h5>Vous devez être connecté pour voir les détails de cet objet</h5><br>
+      <button id="iHaveAnAccountButton" type="submit" class="btn btn-primary" >J'ai déjà un compte</button>
+      <button id="iDontHaveAnAccountButton" type="submit" class="btn btn-danger" >Je veux créer un compte</button>
     </div>
   </div>
 `;
@@ -25,6 +35,7 @@ const HomePage = async () => {
   const items = await getAllItemsByOfferStatus("donated");
   let tbody = document.querySelector("#all_offered_items");
   tbody.innerHTML = getShowItemsHtml(items);
+  checkIfMemberLoggedIn("#homePageModal", "#homePageModalCloseButton");
 
   //Searching an item
   //Listener pour chaque frappe au clavier
