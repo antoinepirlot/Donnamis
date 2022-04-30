f0896029dd40faca4ed88ab85d2db8d77e073a64
Antoine Pirlot <antone.pirlot@student.vinci.be>
2022-04-30 11:37:24 +0200

    Refactor: extract search method for items

--- webapp/src/Components/Pages/Public/HomePage.js
+++ webapp/src/Components/Pages/Public/HomePage.js
@@ -1,6 +1,7 @@
 import {getAllItemsByOfferStatus} from "../../../utils/BackEndRequests";
 import {checkIfMemberLoggedIn, getShowItemsHtml} from "../../../utils/HtmlCode";
 import {getPayload} from "../../../utils/session";
+import {createItemsSearchBar} from "../../../utils/Search";
 
 const tableHtml = `
   <div>
@@ -25,45 +26,18 @@ const tableHtml = `
   </div>
 `;
 
-const searchBarHtml = `
-  <input class="form-control me-2 w-75 d-inline-block" id="searchInput" type="search" placeholder="Rechercher un objet" aria-label="Rechercher"> 
-`;
-
 const HomePage = async () => {
   const pageDiv = document.querySelector("#page");
   pageDiv.innerHTML = tableHtml;
   const items = await getAllItemsByOfferStatus("donated");
-  let tbody = document.querySelector("#all_offered_items");
+  const tbody = document.querySelector("#all_offered_items");
   tbody.innerHTML = getShowItemsHtml(items);
   checkIfMemberLoggedIn("#homePageModal", "#homePageModalCloseButton");
 
   //Searching an item
   //Listener pour chaque frappe au clavier
   if (getPayload()) {
-    const searchBar = document.querySelector("#searchBarHomePage");
-    searchBar.innerHTML = searchBarHtml;
-    const searchInput = document.getElementById('searchInput');
-    searchInput.addEventListener('keyup', function () {
-      //'he' is a library to decode HTML element from a string
-      let he = require('he');
-      //Empty the table
-      tbody.innerHTML = "";
-
-      const input = searchInput.value.toLowerCase().trim();
-      const result = items.filter(
-          item => he.decode(item.member.firstName).toLowerCase().includes(input)
-              || he.decode(item.member.lastName).toLowerCase().includes(input)
-              || he.decode(item.itemType.itemType).toLowerCase().includes(input)
-              || he.decode(item.offerStatus).toLowerCase().includes(input)
-              || he.decode(item.itemDescription).toLowerCase().includes(input)
-      );
-
-      if (result.length < 1) {
-        tbody.innerHTML = `<h1 class="display-6" id="SearchErrorMessage">Il n'y a aucun résultat pour cette recherche</h1>`;
-      } else {
-        tbody.innerHTML = getShowItemsHtml(result);
-      }
-    })
+    createItemsSearchBar(items, "#searchBarHomePage", "#all_offered_items");
   }
 }
 
