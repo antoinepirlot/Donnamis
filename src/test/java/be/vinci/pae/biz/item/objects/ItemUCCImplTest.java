0adf7fe43c1b96aa11e81728f9d305de8db59293
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-04-30 14:22:33 +0200

    Refactor: complete tests for getOneItem

--- src/test/java/be/vinci/pae/biz/item/objects/ItemUCCImplTest.java
+++ src/test/java/be/vinci/pae/biz/item/objects/ItemUCCImplTest.java
@@ -1,6 +1,7 @@
 package be.vinci.pae.biz.item.objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import be.vinci.pae.biz.item.interfaces.ItemDTO;
@@ -40,8 +41,23 @@ class ItemUCCImplTest {
 
   }
 
-  private void setGetAllItemsWorksFine(String offerStatus) {
-    Mockito.when(this.itemDAO.getAllItems(offerStatus)).thenReturn(itemDTOList);
+  private void setGetAllItemsReturnedValue(String offerStatus) {
+    if (offerStatus == null
+        || offerStatus.equals("donated") || offerStatus.equals("assigned")
+        || offerStatus.equals("cancelled") || offerStatus.equals("given")
+    ) {
+      Mockito.when(this.itemDAO.getAllItems(offerStatus)).thenReturn(itemDTOList);
+    } else {
+      Mockito.when(this.itemDAO.getAllItems(offerStatus)).thenReturn(null);
+    }
+  }
+
+  private void setGetOneItemReturnedValue(int idItem) {
+    if (idItem >= 1) {
+      Mockito.when(this.itemDAO.getOneItem(idItem)).thenReturn(this.itemDTO);
+    } else {
+      Mockito.when(this.itemDAO.getOneItem(idItem)).thenReturn(null);
+    }
   }
 
   private void setErrrorDALServiceStart() {
@@ -63,36 +79,43 @@ class ItemUCCImplTest {
   @DisplayName("Test get all items with null offer status")
   @Test
   void testGetAllItemsWithNullOfferStatus() {
-    this.setGetAllItemsWorksFine(null);
+    this.setGetAllItemsReturnedValue(null);
     assertEquals(this.itemDTOList, this.itemUCC.getAllItems(null));
   }
 
   @DisplayName("Test get all items with donated offer status")
   @Test
   void testGetAllItemsWithDonatedOfferStatus() {
-    this.setGetAllItemsWorksFine("donated");
+    this.setGetAllItemsReturnedValue("donated");
     assertEquals(this.itemDTOList, this.itemUCC.getAllItems("donated"));
   }
 
   @DisplayName("Test get all items with assigned offer status")
   @Test
   void testGetAllItemsWithAssignedOfferStatus() {
-    this.setGetAllItemsWorksFine("assigned");
+    this.setGetAllItemsReturnedValue("assigned");
     assertEquals(this.itemDTOList, this.itemUCC.getAllItems("assigned"));
   }
 
   @DisplayName("Test get all items with cancelled offer status")
   @Test
   void testGetAllItemsWithCancelledOfferStatus() {
-    this.setGetAllItemsWorksFine("cancelled");
+    this.setGetAllItemsReturnedValue("cancelled");
     assertEquals(this.itemDTOList, this.itemUCC.getAllItems("cancelled"));
   }
 
   @DisplayName("Test get all items with given offer status")
   @Test
   void testGetAllItemsWithGivenOfferStatus() {
-    this.setGetAllItemsWorksFine("given");
+    this.setGetAllItemsReturnedValue("given");
     assertEquals(this.itemDTOList, this.itemUCC.getAllItems("given"));
+  }
+
+  @DisplayName("Test get all items with not valid offer status")
+  @Test
+  void testGetAllItemsWithNotValidStatus() {
+    this.setGetAllItemsReturnedValue("not valid status");
+    assertNull(this.itemUCC.getAllItems("not valid status"));
   }
 
   @DisplayName("Test get all items with start throwing sql exception")
@@ -109,8 +132,32 @@ class ItemUCCImplTest {
     assertThrows(FatalException.class, () -> this.itemUCC.getAllItems(null));
   }
 
+  @DisplayName("Test get one item with existing id")
   @Test
-  void getOneItem() {
+  void testGetOneItemWithGoodId() {
+    this.setGetOneItemReturnedValue(4);
+    assertEquals(this.itemDTO, this.itemUCC.getOneItem(4));
+  }
+
+  @DisplayName("Test get one item with not existing id")
+  @Test
+  void testGetOneItemWithNotExistingId() {
+    this.setGetOneItemReturnedValue(-1);
+    assertNull(this.itemUCC.getOneItem(-1));
+  }
+
+  @DisplayName("Test get one item with start throwing sql exception")
+  @Test
+  void testGetOneItemWithStartThrowingSQLException() {
+    this.setErrrorDALServiceStart();
+    assertThrows(FatalException.class, () -> this.itemUCC.getOneItem(4));
+  }
+
+  @DisplayName("Test get one item with commit throwing sql exception")
+  @Test
+  void testGetOneItemWithCommitThrowingSQLException() {
+    this.setErrorDALServiceCommit();
+    assertThrows(FatalException.class, () -> this.itemUCC.getOneItem(4));
   }
 
   @Test
