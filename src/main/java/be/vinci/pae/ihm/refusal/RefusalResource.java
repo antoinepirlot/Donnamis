1d4365b89da6b20bb81d0afbeb714c190775e223
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-05-05 21:40:23 +0200

    Fix: get refusal is no more member only

--- src/main/java/be/vinci/pae/ihm/refusal/RefusalResource.java
+++ src/main/java/be/vinci/pae/ihm/refusal/RefusalResource.java
@@ -4,7 +4,6 @@ import be.vinci.pae.biz.refusal.interfaces.RefusalDTO;
 import be.vinci.pae.biz.refusal.interfaces.RefusalUCC;
 import be.vinci.pae.exceptions.webapplication.ObjectNotFoundException;
 import be.vinci.pae.exceptions.webapplication.WrongBodyDataException;
-import be.vinci.pae.ihm.filter.AuthorizeMember;
 import be.vinci.pae.ihm.filter.utils.Json;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -17,7 +16,6 @@ import jakarta.ws.rs.core.MediaType;
 
 @Singleton
 @Path("refusals")
-@AuthorizeMember
 public class RefusalResource {
 
   private final Json<RefusalDTO> json = new Json<>(RefusalDTO.class);
