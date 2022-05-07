677caa153d0d6836bc9f66597bdd8504ea13306e
Victor Denis <victor.denis@student.vinci.com>
2022-05-07 21:26:11 +0200

    Style: member page

--- webapp/src/Components/Pages/Admins/MemberPage.js
+++ webapp/src/Components/Pages/Admins/MemberPage.js
@@ -12,18 +12,19 @@ import {Redirect} from "../../Router/Router";
 import {isAdmin} from "../../../utils/session";
 
 const memberPageHtml = `
-  <div class="form">
-    <div id="memberPageContent" class="d-flex bd-highlight mb-3 shadow-lg p-3 mb-5 bg-white rounded">
+  <div class="whiteCard">
+    <div id="memberPageContent" ><!--class="d-flex bd-highlight mb-3 shadow-lg p-3 mb-5 bg-white rounded" -->
     </div>
-  </div>
-  <div id="errorMessage"></div>
-  <div id="donatedItemsMemberPage">
+    <div id="donatedItemsMemberPage">
     <h4>Objets offerts par ce membre</h4>
     <div id="donatedItemsMemberPageMessage"></div>
   </div>
   <div id="receivedItemsMemberPage">
     <h4>Objets reçus par ce membre</h4>
   <div id="receivedItemsMemberPageMessage"></div>
+  </div>
+  <div id="errorMessage"></div>
+  
 `;
 
 let idMember;
@@ -74,26 +75,33 @@ async function showReceivedItems(member) {
 async function showMemberInformation(member) {
   const content = document.querySelector("#memberPageContent");
   let contentHtml = `
-    <div id="left" class="mr-auto p-2 bd-highlight" xmlns="http://www.w3.org/1999/html">
-      <h3>Infos de ${he.decode(member.username)}</h3>
-      <label>Prénom: </label>
-      <p class="flex-box">${he.decode(member.firstName)}</p>
-      Nom : ${he.decode(member.lastName)}<br>
-      Statut : ${getActualState(member)}<br>
-      Administrateur : ${member.isAdmin ? "Oui" : "Non"}<br>
-    </div>
-    <div id="right" class="p-2 bd-highlight">
-      <h3>Adresse de ${he.decode(member.username)}</h3>
-      ${getAddressHtml(member.address)}<br>
-    </div>
-      Numéro de téléphone : ${member.phoneNumber ? member.phoneNumber : "Aucun"}<br>
-      Nombre d'objets offerts : ${await getNumberOfItems(member.id, "donated")}<br>
-      Nombre d'objets donnés : ${await getNumberOfItems(member.id, "given")}<br>
-      Nombre d'objets intéréssé mais non reçu : ${await getNumberOfReceivedOrNotReceivedItems(
-      member.id, false)}<br>
-      Nombre d'objets reçus : ${await getNumberOfReceivedOrNotReceivedItems(
-      member.id, true)}<br>
-  `;
+    <h3>Infos de ${he.decode(member.username)}</h3>
+    <p>Prénom : <span id="firstnameMemberPage" >${he.decode(member.lastName)}</span> </p>
+    <p >Nom : <span id="lastnameMemberPage">${he.decode(member.lastName)}</span></p>
+    <p >Statut : <span id="statusMemberPage">${getActualState(member)}</span> </p>
+    <p>Administrateur : <span id="adminMemberPage">${member.isAdmin ? "Oui" : "Non"}</span></p>
+    <h3>Adresse de ${he.decode(member.username)}</h3>
+  `
+  //  <div id="left" class="" xmlns="http://www.w3.org/1999/html">
+  //    <h3>Infos de ${he.decode(member.username)}</h3>
+  //    <label>Prénom: </label>
+  //    <p class="">${he.decode(member.firstName)}</p>
+  //    Nom : ${he.decode(member.lastName)}<br>
+  //    Statut : ${getActualState(member)}<br>
+  //    Administrateur : ${member.isAdmin ? "Oui" : "Non"}<br>
+  //  </div>
+  //  <div id="right" class="p-2 bd-highlight">
+  //    <h3>Adresse de ${he.decode(member.username)}</h3>
+  //    ${getAddressHtml(member.address)}<br>
+  //  </div>
+  //    Numéro de téléphone : ${member.phoneNumber ? member.phoneNumber : "Aucun"}<br>
+  //    Nombre d'objets offerts : ${await getNumberOfItems(member.id, "donated")}<br>
+  //    Nombre d'objets donnés : ${await getNumberOfItems(member.id, "given")}<br>
+  //    Nombre d'objets intéréssé mais non reçu : ${await getNumberOfReceivedOrNotReceivedItems(
+  //    member.id, false)}<br>
+  //    Nombre d'objets reçus : ${await getNumberOfReceivedOrNotReceivedItems(
+  //    member.id, true)}<br>
+  //`;
   content.innerHTML += contentHtml;
   const pageErrorDiv = document.querySelector("#errorMessage");
   let button;
